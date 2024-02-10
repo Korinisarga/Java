1e1650466ccd2f7f61e167dd42bf65ec4286eefe
Nisarga kori <137990367+Korinisarga@users.noreply.github.com>
2024-02-10 12:39:28 +0530

    Create patteren_2.java

---
+++ patteren_2.java
@@ -0,0 +1,27 @@
+  /*
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
+*/
+    public class patteren_2{
+
+    public static void main(String[] args) {
+        patter(5);
+        
+    }
+    static void  patter(int n){
+     for(int r=1;r<n;r++)
+     {
+     for(int j=1;j<=n-r+1;j++){
+     System.out.print("*");
+
+     }
+     System.out.println();
+        }
+
+    }
+        
+            
+        }
