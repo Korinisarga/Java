79ddf7e633a61e411013a88906f0057ba765236e
Nisarga kori <137990367+Korinisarga@users.noreply.github.com>
2024-02-10 13:19:43 +0530

    Create pattern_3.java

---
+++ pattern_3.java
@@ -0,0 +1,25 @@
+ /* 1
+    12
+    123
+    1234
+    12345*/
+    public class pattern_3 {
+
+    public static void main(String[] args) {
+        patter(5);
+        
+    }
+    static void  patter(int n){
+     for(int r=1;r<n;r++)
+     {
+     for(int j=1;j<=r;j++){
+       System.out.print(j);
+
+     }
+     System.out.println( " ");
+        }
+
+    }
+        
+            
+        }
